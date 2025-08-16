426d14cebf2f11925281eb4279681024a07ddee5
Ali-085 <m.ali.zahid085@gmail.com>
2025-08-16 17:51:49 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static("dist"))
-
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 // Serve React app for all non-API routes (SPA fallback)
